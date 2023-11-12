3869475d35eed7f8de5401927502ab92fd13f2d4
Artem-Shabaltun <134101240+Artem-Shabaltun@users.noreply.github.com>
2023-11-12 20:52:37 +0200

    hw8
    ready

---
+++ part_1/seeds.py
@@ -0,0 +1,16 @@
+import json
+
+from part_1.models import Quote, Author
+
+if __name__ == '__main__':
+    with open('authors.json', encoding='utf-8') as file:
+        authors = json.load(file)
+        for author in authors:
+            Author(**author).save()
+
+    with open('quotes.json', encoding='utf-8') as file:
+        quotes = json.load(file)
+        for quote in quotes:
+            author = Author.objects(fullname=quote.pop('author')).first()
+            Quote(author=author, **quote).save()
+
